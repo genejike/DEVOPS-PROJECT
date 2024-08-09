60e961b5e8ce9f38e6cb92337e5da8da22b6f312
genejike <genejike2014@gmail.com>
2024-08-09 19:18:37 +0100

    added output for public ip

--- AWS-PROJECTS/Terraform/EC2instance/outputs.tf
+++ AWS-PROJECTS/Terraform/EC2instance/outputs.tf
@@ -1,3 +1,9 @@
+# output "public_ip" {
+#     description = "The public ip address of the web server"
+#     value = aws_instance.terraformer.public_ip
+#     sensitive = false
+  
+# }
 output "alb_dns_name" {
     description = "The domain mane  of the web server"
     value = aws_lb.terraformer.dns_name
